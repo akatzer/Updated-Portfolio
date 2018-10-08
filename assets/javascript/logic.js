34eac0b6183901ed05eb98391650f1c9cd0c77e4
akatzer <akatzer@live.com>
2018-10-08 16:42:55 -0500

    initial commit

---
+++ assets/javascript/logic.js
@@ -0,0 +1,87 @@
+$(document).ready(function () {
+
+    var config = {
+        apiKey: "AIzaSyClw6AQqfS-oSoqt49CeelF5MYKvPezJeg",
+        authDomain: "portfolio-projects-ab6a0.firebaseapp.com",
+        databaseURL: "https://portfolio-projects-ab6a0.firebaseio.com",
+        projectId: "portfolio-projects-ab6a0",
+        storageBucket: "portfolio-projects-ab6a0.appspot.com",
+        messagingSenderId: "161578837683"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+    var uploader = document.getElementById("uploader")
+    var fileButton = document.getElementById("fileButton")
+
+  fileButton.addEventListener('change', function (e) {
+        //get file
+        var file = e.target.files[0];
+        //create a store ref
+        var storageRef = firebase.storage().ref('images/' + file.name);
+
+        function getUrl() {
+            storageRef.getDownloadURL().then(function (url) {
+                $("#imageUrl").html(url);
+            });
+        }
+
+        //upload file
+        var task = storageRef.put(file);
+
+        // update progress bar
+        task.on('state_changed',
+            function progress(snapshot) {
+                var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                uploader.value = percentage;
+            },
+            function error(err) {
+            },
+            function complete() {
+                getUrl();
+                
+            }
+        );
+    });
+
+    $("#submit").on("click", function (event) {
+                    
+        event.preventDefault();
+        var projectName = $("#projectName").val().trim();
+        var gitHubLink = $("#gitHubLink").val().trim();
+        var imageUrl = $("#imageUrl").text();
+
+        //creating the variable object that will push to the database
+        var newProject = {
+            projectName: projectName,
+            imageUrl: imageUrl,
+            gitHubLink: gitHubLink,
+        };
+
+        //pushes new input into the database
+        if (projectName === "" && gitHubLink === ""()) {
+        }
+        else {
+            database.ref().push(newProject);
+        }
+
+        //clears the form out for the next entry
+        $(".form-control").val("");
+        $("#fileButton").val("");
+        $("#imageUrl").text("");
+        $("#uploader").val("0")
+       
+    })
+
+   
+
+  
+
+
+
+
+
+
+
+})
---
+++ assets/javascript/new-project.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  
+    var config = {
+        apiKey: "AIzaSyClw6AQqfS-oSoqt49CeelF5MYKvPezJeg",
+        authDomain: "portfolio-projects-ab6a0.firebaseapp.com",
+        databaseURL: "https://portfolio-projects-ab6a0.firebaseio.com",
+        projectId: "portfolio-projects-ab6a0",
+        storageBucket: "portfolio-projects-ab6a0.appspot.com",
+        messagingSenderId: "161578837683"
+    };
+    firebase.initializeApp(config);
+
+    var database = firebase.database();
+
+  //creates the snapshot when a new data entry is loaded into firebase
+  database.ref().on("child_added", function (childSnapshot) {
+
+    //creation of variables based on the snapshot of the database
+    var projectName = childSnapshot.val().projectName;
+    var gitHubLink = childSnapshot.val().gitHubLink;
+    var imageUrl = childSnapshot.val().imageUrl;
+
+
+var newCard = $("<div class='card' style='width: 18rem;'>").append(
+    $('<img class="card-img-top" src="' + imageUrl + '" alt="Project Image">'),
+    
+    $('<h5 id="project-name">').text(projectName),
+    $('<a href="' + gitHubLink + '" class="btn btn-primary">Check it out!</a>')
+)
+
+$(".newCard").prepend(newCard);
+
+});
+
+})
